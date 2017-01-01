155b5faac246ac2a29d909087ab22ec21ba4d36b
hsuting <hsuting0106@gmail.com>
2017-01-02 06:14:40 +0800

    fix ubuntu nodejs

--- nodejs.sh
+++ nodejs.sh
@@ -9,6 +9,9 @@ case $system in
       curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
       echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
       sudo apt-get update
+
+      curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+      check_command nodejs
     fi
     ;;
 esac
