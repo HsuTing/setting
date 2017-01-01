9b587a6454988a8fa4636ae526ce8dc9c932caf2
hsuting <hsuting0106@gmail.com>
2016-12-31 20:58:08 -0800

    fix vim plugin bug

--- mac.sh
+++ mac.sh
@@ -9,7 +9,7 @@ if ! type "brew" > /dev/null 2>&1; then
   ## add brew
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-  brew install readline
-  brew install tree
-  brew install bash-completion
+  check_command readline
+  check_command tree
+  check_command bash-completion
 fi
--- vim.sh
+++ vim.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+. setting.sh
 . command.sh
 
 check_command git
 check_command vim
+check_command cmake
+
+case $system in
+  ($linux)
+    check_command python-dev
+    ;;
+esac
 
 if [ ! -e ~/.vim/bundle/Vundle.vim ]; then
   print_command \
-  "add '.vimrc' and vim plugin"
+  "add '.vimrc' and vim plugins"
 
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
   ln vimrc ~/.vimrc
   vim +PluginInstall +qall
+
+  ## run other setting of vim plugins
   cp -R ~/.vim/bundle/vim-colorschemes/colors/ ~/.vim
+  ~/.vim/bundle/YouCompleteMe/install.py --tern-completer
 fi
