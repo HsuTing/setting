367837b1478b461387eff2660cb458a7a2093d2a
hsuting <hsuting0106@gmail.com>
2017-01-28 07:17:31 -0800

    add docker

--- linux.sh
+++ linux.sh
@@ -8,3 +8,27 @@ sudo chown -R $USER /var/www/html
 
 ## set tern for vim
 cp ./tern-project /var/www/html/.tern-project
+
+## install docker
+if ! type "docker" > /dev/null 2>&1; then
+  print_command \
+  "Install docker"
+
+  sudo apt-get install curl \
+                       linux-image-extra-$(uname -r) \
+                       linux-image-extra-virtual
+
+  sudo apt-get install apt-transport-https \
+                       ca-certificates
+
+  curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+
+  sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+        ubuntu-$(lsb_release -cs) \
+        main"
+
+  sudo apt-get update
+
+  sudo apt-get -y install docker-engine
+fi
--- mac.sh
+++ mac.sh
@@ -8,3 +8,9 @@ check_command bash-completion
 
 ## set tern for vim
 cp ./tern-project /Library/WebServer/Documents/.tern-project
+
+## Install docker
+if ! type "docker" > /dev/null 2>&1; then
+  print_command \
+  "Install docker: https://docs.docker.com/docker-for-mac/"
+fi
