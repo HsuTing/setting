f20d44ccced5d29a83f6632e6d5b244cd6da9e1f
hsuting <hsuting0106@gmail.com>
2017-04-06 09:43:06 +0000

    check command

--- command.sh
+++ command.sh
@@ -16,7 +16,7 @@ exec_command () {
 }
 
 install_command() {
-  if [ ! type "sudo" > /dev/null 2>&1 ]; then
+  if ! type "sudo" > /dev/null 2>&1; then
     $@
   else
     sudo $@
@@ -24,7 +24,7 @@ install_command() {
 }
 
 check_command () {
-  if [ ! type "$1" > /dev/null 2>&1 ]; then
+  if ! type "$1" > /dev/null 2>&1; then
     case $system in
       ($linux)
         exec_command \
@@ -43,37 +43,26 @@ check_command () {
   fi
 }
 
-check_command_string() {
-  count=1
-  for string in $@; do
-    printf "${string}"
-    if [ $count != $# ]; then
-      printf " "
-    fi
-    count=$((count + 1))
-  done
-}
-
 check_command_exist() {
-  if [ ! type "$@" > /dev/null 2>&1 ]; then
-    printf "${red}[checker] command not find: $(check_command_string $@).${nocolor}\n"
+  if ! type "$@" > /dev/null 2>&1; then
+    printf "${red}[checker] command not find: $1.${nocolor}\n"
   else
-    printf "${cyan}[checker] command \"$(check_command_string $@)\" is right.${nocolor}\n"
+    printf "${cyan}[checker] command \"$1\" is right.${nocolor}\n"
   fi
 }
 
 check_folder_exist() {
   if [ ! -e "$@" ]; then
-    printf "${red}[checker] folder not find: $(check_command_string $@).${nocolor}\n"
+    printf "${red}[checker] folder not find: $1.${nocolor}\n"
   else
-    printf "${cyan}[checker] folder \"$(check_command_string $@)\" exits.${nocolor}\n"
+    printf "${cyan}[checker] folder \"$1\" exits.${nocolor}\n"
   fi
 }
 
 check_file_exist() {
   if [ ! -f "$@" ]; then
-    printf "${red}[checker] file not find: $(check_command_string $@).${nocolor}\n"
+    printf "${red}[checker] file not find: $1.${nocolor}\n"
   else
-    printf "${cyan}[checker] file \"$(check_command_string $@)\" exits.${nocolor}\n"
+    printf "${cyan}[checker] file \"$1\" exits.${nocolor}\n"
   fi
 }
