123bc6ad29021e80b21c8c9436ca410830fcffb9
hsuting <hsuting0106@gmail.com>
2017-05-19 14:11:56 +0800

    add server

---
+++ bash/server.sh
@@ -0,0 +1,50 @@
+run-server () {
+  case $1 in
+    "--run")
+      if [ -n "$2" ]; then
+        sh ~/Desktop/server/$2.sh
+      else
+        echo "Need to give a name."
+      fi
+      ;;
+
+    "--make")
+      if [ -n "$2" ]; then
+        cp ~/setting/server/template-server.sh ~/Desktop/server/$2.sh
+        cd ~/Desktop/server
+      else
+        echo "Need to give a name."
+      fi
+      ;;
+
+    "--goto")
+      cd ~/Desktop/server
+      ;;
+  esac
+}
+
+_runServer () {
+  local cur prev opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  opts="--run --make --goto"
+
+  if [ ${prev} == run-server ]; then
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+  elif [ ${prev} == --run ]; then
+    opts=""
+    for entry in "${HOME}/Desktop/server"/*; do
+      filename=$(basename "$entry")
+      filename="${filename%.*}"
+      if [ $filename == key ]; then
+        continue
+      fi
+      opts+=" $filename"
+    done
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+  fi
+}
+complete -F _runServer run-server
