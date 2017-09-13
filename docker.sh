d9e68eeb10ffc20e7fdca24dc766536e98ea830a
HsuTing <hsuting0106@gmail.com>
2017-09-13 23:07:36 +0800

    modify docker

--- docker.sh
+++ docker.sh
@@ -16,14 +16,10 @@ if ! type "docker" > /dev/null 2>&1; then
       software-properties-common
 
       curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+      apt-key fingerprint 0EBFCD88
 
       install_command add-apt-repository \
       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-       $(lsb_release -cs) \
-      stable"
-
-      install_command add-apt-repository \
-      "deb [arch=armhf] https://download.docker.com/linux/ubuntu \
       $(lsb_release -cs) \
       stable"
 
