a6d85e8ada9478a30a0f1b66497d718767700490
HsuTing <hsuting0106@gmail.com>
2018-01-20 20:07:41 +0800

    feat(bash): modify make-log

--- bash/makeLog.sh
+++ bash/makeLog.sh
@@ -1,15 +1,15 @@
 . ~/setting/setting.sh
 
+alias _makeLog="conventional-changelog -p angular -i CHANGELOG.md -s -r 0"
 make-log() {
   if [ ! -z "$1" ]; then
-    git tag $1
-    conventional-changelog -p angular -i CHANGELOG.md -s -r 0
-    git add .
-    git commit -m "make CHANGELOG.md"
-    git rebase -i HEAD~2
-    git tag -d $1
-    git tag -a $1
-    conventional-changelog -p angular -i CHANGELOG.md -s -r 0
+    git tag $1 && \
+    _makeLog && \
+    git add CHANGELOG.md && \
+    git commit --amend  --no-edit && \
+    git tag -d $1 && \
+    git tag -a $1 && \
+    _makeLog
   else
     printf "${red}(✘)${nocolor} You need to give a tag\n"
   fi
